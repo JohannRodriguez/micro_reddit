7017a923a124a539be29097f13ca03679af6f4bf
Johann <themrblacksapce@hotmail.com>
2020-05-08 16:05:43 -0600

    Correct maximum lengths

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    validates :body, presence: true, length: { maximum: 250 }
+    validates :body, presence: true, length: { maximum: 260 }
     validates :user_id, presence: true
     validates :post_id, presence: true
     belongs_to :user
