1035e8333808a19a73c44d240e7a5d2951eb501d
carlosprietobarron <carlosprietobarron@gmail.com>
2020-05-11 13:04:14 -0700

    Add and configure linters and fix some linter errs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :user_name, presence: true, length: { minimum: 3, maximum: 10}
-    validates_uniqueness_of :user_name
-    validates :password, presence: true, length: { minimum: 5, maximum: 20}
-    has_many :comments
-    has_many :posts
+  validates :user_name, presence: true, length: { minimum: 3, maximum: 10 }
+  validates_uniqueness_of :user_name
+  validates :password, presence: true, length: { minimum: 5, maximum: 20 }
+  has_many :comments
+  has_many :posts
 end
